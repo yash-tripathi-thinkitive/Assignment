6af34cedf4d87049e8380acf16d4af538ff2b38d
Yash Tripathi <yash.tripathi@thinkitive.com>
2021-02-05 18:06:26 +0530

    added spring rest assign

---
+++ SpringRestAssignment/SpringRESTAssignment/src/main/java/com/thinkitive/EmployeeRepo.java
@@ -0,0 +1,33 @@
+package com.thinkitive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeRepo {
+	List<Employee> list = new ArrayList<Employee>();
+
+	public void insert(Employee e) {
+		list.add(e);
+	}
+
+	public void delete(Employee e) {
+		list.remove(e);
+	}
+
+	public void update(Employee e) {
+		list.set(list.indexOf(e), e);
+	}
+
+	public List<Employee> getAll() {
+		return list;
+	}
+
+	public Employee getEmployee(Integer empid) {
+		Employee temp = null;
+		for (Employee e : list) {
+			if (e.getEmpid() == empid)
+				temp = e;
+		}
+		return temp;
+	}
+}
